8bd0a8bad87158ed7204500851c066e8a42553bd
Lihongbo0512 <37478514+Lihongbo0512@users.noreply.github.com>
2019-07-14 20:23:18 +0800

    fx to hlsl

--- BlendDemo/Vertex.h
+++ BlendDemo/Vertex.h
@@ -18,34 +18,13 @@ namespace Vertex
 		XMFLOAT3 Normal;
 		XMFLOAT2 Tex;
 	};
-
-	struct PosNormalTexTan
-	{
-		XMFLOAT3 Pos;
-		XMFLOAT3 Normal;
-		XMFLOAT2 Tex;
-		XMFLOAT4 TangentU;
-	};
-
-	struct PosNormalTexTanSkinned
-	{
-		XMFLOAT3 Pos;
-		XMFLOAT3 Normal;
-		XMFLOAT2 Tex;
-		XMFLOAT4 TangentU;
-		XMFLOAT3 Weights;
-		BYTE BoneIndices[4];
-	};
 }
 
 class InputLayoutDesc
 {
 public:
 	// Init like const int A::a[4] = {0, 1, 2, 3}; in .cpp file.
-	static const D3D11_INPUT_ELEMENT_DESC Pos[1];
 	static const D3D11_INPUT_ELEMENT_DESC Basic32[3];
-	static const D3D11_INPUT_ELEMENT_DESC PosNormalTexTan[4];
-	static const D3D11_INPUT_ELEMENT_DESC PosNormalTexTanSkinned[6];
 };
 
 class InputLayouts
@@ -54,10 +33,7 @@ public:
 	static void InitAll(ID3D11Device* device);
 	static void DestroyAll();
 
-	static ID3D11InputLayout* Pos;
 	static ID3D11InputLayout* Basic32;
-	static ID3D11InputLayout* PosNormalTexTan;
-	static ID3D11InputLayout* PosNormalTexTanSkinned;
 };
 
 #endif // VERTEX_H
